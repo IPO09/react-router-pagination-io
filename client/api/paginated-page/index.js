ba9a295cb171453a705dc59e2f57038b8873365e
TOKYO <nezexmain@gmail.com>
2022-10-06 23:05:58 +0200

    'ready'

--- client/api/paginated-page/index.js
+++ client/api/paginated-page/index.js
@@ -1,3 +1,11 @@
 import fetch from 'isomorphic-fetch'
+const API_URL = 'https://reqres.in/api/';
 
-export const paginatedPage = ({ page }) => fetch(`/api/${page}`).then((response) => response.json())
+export const paginatedPage = (page) => {
+  return fetch(`${API_URL}users?page=${page}`)
+    .then(response => response.json())
+    .catch(() => ({
+      Response: 'False',
+      Error: 'Page not found',
+    }));
+};
